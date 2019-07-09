9003a5bedc28282796017ef8e1a1994051aca2b8
markhilb <mhilbertsen@hotmail.no>
2019-07-09 12:32:27 +0200

    Initial commit

---
+++ program.py
@@ -0,0 +1,8 @@
+import tkinter
+# https://www.geeksforgeeks.org/python-gui-tkinter/
+
+mainWindow = tkinter.Tk(screenName="screenname", baseName="basename", className="classname", useTk=1)
+
+
+
+mainWindow.mainloop()
