c4bbd47b9aae6fb9b105d96947a522b0cda58289
markhilb <mhilbertsen@hotmail.no>
2020-06-20 22:51:57 +0200

    Update to new version

--- AvskjermingGUI/src/setup.py
+++ AvskjermingGUI/src/setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["tkinter", "decimal"]}
+build_exe_options = {"packages": ["tkinter", "decimal", "re"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -10,9 +10,9 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  
+setup(
     name = "Vindskjerming",
-    version = "2.0",
+    version = "3.0",
     description = "GUI for Vindskjerming!",
     options = {"build_exe": build_exe_options},
     executables = [Executable("main.py", base=base, targetName="Vindskjerming")]
