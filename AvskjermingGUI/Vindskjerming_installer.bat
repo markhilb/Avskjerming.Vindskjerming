a6746d1f87157ca03a5dfe7a153c2445a6ba34d1
mhilb <mhilb@LAPTOP-MADV11MC>
2020-07-19 18:17:09 +0200

    Fix installer :)

--- AvskjermingGUI/Vindskjerming_installer.bat
+++ AvskjermingGUI/Vindskjerming_installer.bat
@@ -9,46 +9,39 @@ set vindskjerming_file="Vindskjerming.msi"
 curl -s -L -X GET https://github.com/markhilb/Avskjerming/raw/export/AvskjermingGUI/src/dist/Vindskjerming-0.0.0-win32.msi -o %vindskjerming_file%
 
 set folder="%userprofile%\AppData\Local\Programs\Vindskjerming"
-IF EXIST "%folder%" (
-    echo Updating Vindskjerming...
-    msiexec /fa %vindskjerming_file% /l*v netfx.log /qn
-    IF NOT "%errorlevel%" == 0 (
-        del /f /q "%folder%\*"
-        rmdir /s /q "%folder%\lib"
-        msiexec /i %folder% /qn
-    )
-) else (
-    echo Installing Vindskjerming...
+echo Updating Vindskjerming...
+msiexec /famus %vindskjerming_file% /l*v netfx.log /qn
+IF NOT %errorlevel% == 0 (
+    del /f /q "%folder%\*"
+    rmdir /s /q "%folder%\lib"
     msiexec /i %vindskjerming_file% /qn
 )
 
-:: del /f %vindskjerming_file%
+del /f %vindskjerming_file%
+del /f netfx.log
 
 
-:: set miktex_file="MiKTeX.exe"
-:: IF NOT EXIST "C:\Program Files\MiKTeX" (
-::     echo Inside...
-::     echo Downloading MiKTeX...
-::     curl -s -L -X GET https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-20.6.29-x64.exe -o %miktex_file%
-::     echo Installing MiKTeX...
-::     %miktex_file% ^
-::         --remote-package-repository=ftp.acc.umu.se ^
-::         --shared ^
-::         --unattended
-::     setx path "%path%;C:\Program Files\MiKTeX\miktex\bin\x64\"
-::     del /f %miktex_file%
-:: )
+set miktex_file="MiKTeX.exe"
+IF NOT EXIST "C:\Program Files\MiKTeX" (
+    echo Downloading MiKTeX...
+    curl -s -L -X GET https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-20.6.29-x64.exe -o %miktex_file%
+    echo Installing MiKTeX...
+    %miktex_file% ^
+        --remote-package-repository=ftp.acc.umu.se ^
+        --shared ^
+        --unattended
+    del /f %miktex_file%
+)
 
 
-:: set ghostscript_file="GhostScript.exe"
-:: IF NOT EXIST "C:\Program Files (x86)\gs" (
-::     echo Downloading GhostScript...
-::     curl -s -L -X GET https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs952/gs952w32.exe -o %ghostscript_file%
-::     echo Installing GhostScript...
-::     %ghostscript_file% /S
-::     setx path "%path%;C:\Program Files (x86)\gs\gs9.52\bin\"
-::     del /f %ghostscript_file%
-:: )
+set ghostscript_file="GhostScript.exe"
+IF NOT EXIST "C:\Program Files (x86)\gs" (
+    echo Downloading GhostScript...
+    curl -s -L -X GET https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs952/gs952w32.exe -o %ghostscript_file%
+    echo Installing GhostScript...
+    %ghostscript_file% /S
+    del /f %ghostscript_file%
+)
 
 
 echo Done!
