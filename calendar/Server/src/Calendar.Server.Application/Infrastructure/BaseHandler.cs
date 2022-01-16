a9987fbe6b8139597b5918d15b0f1894cc2add10
Markus Bergum Hilbertsen <markus@hilbertsen.com>
2022-01-16 09:31:48 +0100

    Add hash function

--- calendar/Server/src/Calendar.Server.Application/Infrastructure/BaseHandler.cs
+++ calendar/Server/src/Calendar.Server.Application/Infrastructure/BaseHandler.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Data.Common;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Calendar.Server.Application.Infrastructure
 {
@@ -12,5 +14,16 @@ namespace Calendar.Server.Application.Infrastructure
 
         public void Dispose() =>
             _db.Close();
+
+        public static string ComputeSHA256Hash(string input)
+        {
+            var bytes = SHA256.Create().ComputeHash(Encoding.ASCII.GetBytes("qGmLwByUv_" + input));
+
+            var result = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+                result.Append(bytes[i].ToString("x2"));
+
+            return result.ToString();
+        }
     }
 }
